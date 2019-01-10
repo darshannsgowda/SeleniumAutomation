3c4aa820f12147f6575e73c8ecaf998ec2acce48
Darshan Nagesh <darshan.n@votercircle.com>
2019-01-10 19:18:13 +0530

    changed

--- src/test/java/com/guru99/custompage/Guru99TestCreationOfCustomer.java
+++ src/test/java/com/guru99/custompage/Guru99TestCreationOfCustomer.java
@@ -3,6 +3,7 @@ package com.guru99.custompage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.guru99.customer.Guru99Customer;
@@ -16,7 +17,8 @@ public class Guru99TestCreationOfCustomer
 	Guru99Login_ResetPassword_LogoutPage Login;
 	
 	
-	@Test(priority = 1, dataProvider="createCustomer")
+	@Test(priority = 1, dataProvider="createCustomer")//
+//	@Parameters({"Usrname","Pwd", "Custmrname", "Dob", "addres","city","state","pin","phone","eml","pass" })
 	public void Creation_of_NewCustomer(String Usrname, String Pwd, String Custmrname, String Dob, String addres, String city,String state, String pin, String phone, String eml, String pass ) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\darsh\\eclipse-workspace\\SeleniumAutomation\\src\\main\\resources\\browser_drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -36,7 +38,7 @@ public class Guru99TestCreationOfCustomer
     public static Object[][] getDataFromCreateCustomer(){
         return new Object[][] {
            
-            { "mngr172590","gApYbUt","bollogger","11-11-1998","Thanks for coming here", "California", "USA", "928392", "9887327382", "hab91100@votercircle.in", "Test123" },
+            { "mngr172590","gApYbUt","bolloger","11-11-1998","Thanks for coming here", "California", "USA", "928392", "9887327382", "hab9100@votercircle.in", "Test123" },
            
         };  
 	  }
